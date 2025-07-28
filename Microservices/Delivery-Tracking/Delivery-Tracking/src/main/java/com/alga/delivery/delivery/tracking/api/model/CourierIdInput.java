40b0f9dca3a9abd83387c90c886194ece16f4660
joaomorito <joaomorito04@gmail.com>
2025-07-27 23:13:28 -0300

    DeliveryController implemented

---
+++ Microservices/Delivery-Tracking/Delivery-Tracking/src/main/java/com/alga/delivery/delivery/tracking/api/model/CourierIdInput.java
@@ -0,0 +1,13 @@
+package com.alga.delivery.delivery.tracking.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class CourierIdInput {
+
+    private UUID courierId;
+}
