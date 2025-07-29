a79a8bf02dcc94194f4ce306a2e29fbe0daa68d6
joaomorito <joaomorito04@gmail.com>
2025-07-29 00:18:22 -0300

    Retry Pattern implemented with Resilience4j

--- Microservices/Delivery-Tracking/Delivery-Tracking/src/main/java/com/alga/delivery/delivery/tracking/infrastructure/http/client/CourierAPIClientConfig.java
+++ Microservices/Delivery-Tracking/Delivery-Tracking/src/main/java/com/alga/delivery/delivery/tracking/infrastructure/http/client/CourierAPIClientConfig.java
@@ -1,6 +1,5 @@
 package com.alga.delivery.delivery.tracking.infrastructure.http.client;
 
-import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchProperties;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
